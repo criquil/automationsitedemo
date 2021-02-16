ad536035d1fc753f0109ad57e73aee2540278bd5
criquil <cristianquil@gmail.com>
2021-02-16 12:18:34 -0300

    remove 0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom0';
+import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 
