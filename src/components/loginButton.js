09e2d9977e45d82c17d9eb59a3fb778867f5f782
criquil <cristianquil@gmail.com>
2021-02-16 12:38:38 -0300

    remove

--- src/components/loginButton.js
+++ src/components/loginButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
-const loginButton = () => {
+const LoginButton = () => {
 const  { loginWithRedirect } = useAuth0();
     return (
         <button onClick={()=>loginWithRedirect()}>
@@ -9,4 +9,4 @@ const  { loginWithRedirect } = useAuth0();
     )
 }
 
-export default loginButton
+export default LoginButton
