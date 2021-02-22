cee122963f401f67b93adfb6f2fdc95e87e57209
criquil <cristianquil@gmail.com>
2021-02-22 12:39:57 -0300

    TestLoading

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-
+import IsLoading from "./isLoading"
 const HomeContent = () => {
     const  { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();  
     if (isLoading) return <div><IsLoading/></div>
